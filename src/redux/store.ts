d9ec7634a58133cadb32d08501935cd447730bbb
Artem <zezebolz@gmail.com>
2023-09-22 09:55:54 +0300

    added redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import timerReducer from "./timer/slice";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const combinedReducers = combineReducers({
+  timer: timerReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
+
 export const store = configureStore({
-  reducer: {
-    timer: timerReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
