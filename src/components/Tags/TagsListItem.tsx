ae1fda51ad73eee983dec7f17961cdccb29799a5
Artem <zezebolz@gmail.com>
2023-10-06 16:07:01 +0300

    added remove tag

--- src/components/Tags/TagsListItem.tsx
+++ src/components/Tags/TagsListItem.tsx
@@ -1,16 +1,30 @@
 import { Tag } from "../../types";
 
+import { RxCross2 } from "react-icons/rx";
+import { useAppDispatch } from "../../redux/store";
+import { removeTag } from "../../redux/tags/slice";
+
 interface TagsListItem {
   tag: Tag;
 }
 
 export const TagsListItem = ({ tag }: TagsListItem) => {
+  const dispatch = useAppDispatch();
+
+  const onClickRemove = () => {
+    dispatch(removeTag(tag));
+  };
+
   return (
-    <li className="mt-[30px] first-[mt-0] text-center px-[50px]">
+    <li className="mt-[30px] first-[mt-0] text-center px-[50px] relative">
       <h5 className="text-[20px] text-primary-dark dark:text-primary">
         {tag.name}
       </h5>
-
+      <RxCross2
+        className="absolute right-[17%] top-1/2 -translate-y-1/2 cursor-pointer"
+        onClick={onClickRemove}
+        size={20}
+      />
       <div className="w-full h-[2px] bg-primary-dark dark:bg-primary" />
     </li>
   );
--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,17 +1,13 @@
-import { motion } from "framer-motion";
 import { useState } from "react";
-import { CiCirclePlus } from "react-icons/ci";
+import { motion } from "framer-motion";
+
 import { useSelector } from "react-redux";
 import { selectTags } from "../../redux/selectors";
-import { useAppDispatch } from "../../redux/store";
-import { Tag } from "../../types";
+
 import { TagPortal } from "../TagPortal";
 import { TagsListItem } from "./TagsListItem";
 
-const testTags = [
-  { name: "Sport", completedRounds: 0 },
-  { name: "Work", completedRounds: 3 },
-];
+import { CiCirclePlus } from "react-icons/ci";
 
 export const Tags = () => {
   const [isPortalOpen, setIsPortalOpen] = useState(false);
@@ -24,8 +20,8 @@ export const Tags = () => {
 
   return (
     <div
-      className="w-full h-[350px] box-border relative  
-      border-[2px] border-primary-dark dark:border-primary rounded-medium flex flex-col"
+      className="w-full h-[350px] box-border relative border-[2px] 
+      border-primary-dark dark:border-primary rounded-medium flex flex-col"
     >
       <button
         className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 bg-primary dark:bg-primary-dark"
