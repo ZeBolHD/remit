20f1cc29e7453685ac17fc45f4d4cc877012251f
Artem <zezebolz@gmail.com>
2023-10-13 11:01:10 +0300

    added responsive sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#DDDDDD",
-        "primary-dark": "#202020",
-        "primary-bg-dark": "#202020",
-        "primary-bg-light": "#DDDDDD",
-        active: "#D77A45",
-      },
-      borderRadius: {
-        large: "50px",
-        medium: "25px",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#DDDDDD",
+				"primary-dark": "#202020",
+				"primary-bg-dark": "#202020",
+				"primary-bg-light": "#DDDDDD",
+				active: "#D77A45",
+			},
+			borderRadius: {
+				large: "50px",
+				medium: "25px",
+			},
+			screens: {
+				mobile: "400px",
+				tablet: "768px",
+				laptop: "1024px",
+			},
+		},
+	},
+	plugins: [],
 };
